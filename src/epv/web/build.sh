0c1e45d5dd8f8a69e6a1f16268e398d39bed554e
FEI YUAN <fei.yuan@bcm.edu>
2026-06-03 12:54:30 -0500

    Improved build.sh for dist

--- src/epv/web/build.sh
+++ src/epv/web/build.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 
+set -e
+
+VERSION=$(git rev-parse --short HEAD)
+
 mkdir -p dist
 cp LICENSE README.md style.css usage.js visualizer.js dist/
-sed -e 's|src="./|src="https://cdn.jsdelivr.net/gh/yuanfeifuzzy/epv@main/src/epv/web/dist/|g' \
-    -e 's|href="./|href="https://cdn.jsdelivr.net/gh/yuanfeifuzzy/epv@main/src/epv/web/dist/|g' \
-    index.html > dist/index.html
+
+sed \
+  -e "s|src=\"./|src=\"https://cdn.jsdelivr.net/gh/yuanfeifuzzy/epv@${VERSION}/src/epv/web/dist/|g" \
+  -e "s|href=\"./|href=\"https://cdn.jsdelivr.net/gh/yuanfeifuzzy/epv@${VERSION}/src/epv/web/dist/|g" \
+  index.html > dist/index.html
+
+git add dist
+git commit -m "Update dist for ${VERSION}"
+git push
